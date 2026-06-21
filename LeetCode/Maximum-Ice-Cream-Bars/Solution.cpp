7845372e55f15d7bd5e680d4597efcbcc0f7ec3f
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-22 00:39:33 +0530

    Add solution for Maximum Ice Cream Bars

--- LeetCode/Maximum-Ice-Cream-Bars/Solution.cpp
+++ LeetCode/Maximum-Ice-Cream-Bars/Solution.cpp
@@ -1,13 +1,19 @@
 1class Solution {
 2public:
 3    int maxIceCream(vector<int>& arr, int c) {
-4        sort(arr.begin(), arr.end());
-5        int tot = 0;
-6        for(int el: arr){
-7            if(el>c) break;
-8            c -= el;
-9            tot++;
-10        }
-11        return tot;
-12    }
-13};
+4        map<int, int> mp;
+5        for(int el: arr){
+6            mp[el]++;
+7        }
+8
+9        int tot = 0;
+10        for(auto el: mp){
+11            if(el.first>c) break;
+12            int cant = min(el.second, c/el.first);
+13            tot += cant;
+14            c -= cant*el.first;
+15        }
+16
+17        return tot;
+18    }
+19};
