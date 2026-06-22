679899d397d58fcd9d0f0f39366015c7c4b50bd7
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-22 15:40:13 +0530

    Add solution for Flatten a Multilevel Doubly Linked List

--- LeetCode/Flatten-a-Multilevel-Doubly-Linked-List/Solution.cpp
+++ LeetCode/Flatten-a-Multilevel-Doubly-Linked-List/Solution.cpp
@@ -11,32 +11,40 @@
 11
 12class Solution {
 13public:
-14    Node* flatten(Node* head) {
-15        if(!head){
-16            return nullptr;
-17        }
-18        stack<Node*> st;
-19        st.push(head);
-20        Node* dumh = new Node(-1);
-21        Node* cur = dumh;
-22        while(!st.empty()){
-23            auto tn = st.top(); st.pop();
-24            
-25            if(tn->next){
-26                st.push(tn->next);
-27            }
-28            if(tn->child){
-29                st.push(tn->child);
-30            }
-31            cur->next = tn;
-32            tn->prev = cur;
-33            cur = cur->next;
-34            cur->child = nullptr;
-35        }
-36
-37        head->prev = nullptr;
-38
-39        return head;
+14    Node* dumh;
+15    Node* cur;
+16    unordered_map<Node*, int> ump;
+17    void rec(Node* head){
+18        if(head == nullptr) return;
+19
+20        // Node* newn = new Node(head->val);
+21        cur->next = head;
+22        head->prev = cur;
+23
+24        cur = cur->next;
+25        
+26        queue<Node*> q;
+27        if(head->child){
+28            q.push(head->child);
+29        }
+30        if(head->next){
+31            q.push(head->next);
+32        }
+33        cur->next = nullptr;
+34        cur->child = nullptr;
+35        while(!q.empty()){
+36            auto tn = q.front(); q.pop();
+37            rec(tn);
+38        }
+39    }
 40
-41    }
-42};
+41    Node* flatten(Node* head) {
+42        dumh = new Node(-1);
+43        cur = dumh;
+44        rec(head);
+45        if(head){
+46            head->prev = nullptr;
+47        }
+48        return head;
+49    }
+50};
