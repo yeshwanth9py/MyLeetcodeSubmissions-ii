e3d4fcdd5162a1d1ee9d06efb75d08b75b409d42
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-22 15:38:18 +0530

    Add solution for Flatten a Multilevel Doubly Linked List

--- LeetCode/Flatten-a-Multilevel-Doubly-Linked-List/Solution.cpp
+++ LeetCode/Flatten-a-Multilevel-Doubly-Linked-List/Solution.cpp
@@ -1,35 +1,42 @@
-1class Solution {
-2public:
-3    Node* flatten(Node* head) {
-4        if (!head) return nullptr;
-5
-6        Node* dummy = new Node(0);  // Dummy node to simplify pointer manipulation
-7        Node* prev = dummy;
-8
-9        flattenDFS(head, prev);
-10
-11        // Detach the dummy node
-12        dummy->next->prev = nullptr;
-13        return dummy->next;
-14    }
-15
-16private:
-17    void flattenDFS(Node* curr, Node*& prev) {
-18        if (!curr) return;
-19
-20        prev->next = curr;
-21        curr->prev = prev;
-22
-23        prev = curr;  // Update prev to the current node
-24
-25        Node* nextNode = curr->next;  // Store next node because it may get overwritten
-26
-27        if (curr->child) {
-28            flattenDFS(curr->child, prev);
-29            curr->child = nullptr;  // Clear the child pointer
-30        }
-31
-32        flattenDFS(nextNode, prev);
-33    }
-34};
-35
+1/*
+2// Definition for a Node.
+3class Node {
+4public:
+5    int val;
+6    Node* prev;
+7    Node* next;
+8    Node* child;
+9};
+10*/
+11
+12class Solution {
+13public:
+14    Node* flatten(Node* head) {
+15        if(!head){
+16            return nullptr;
+17        }
+18        stack<Node*> st;
+19        st.push(head);
+20        Node* dumh = new Node(-1);
+21        Node* cur = dumh;
+22        while(!st.empty()){
+23            auto tn = st.top(); st.pop();
+24            
+25            if(tn->next){
+26                st.push(tn->next);
+27            }
+28            if(tn->child){
+29                st.push(tn->child);
+30            }
+31            cur->next = tn;
+32            tn->prev = cur;
+33            cur = cur->next;
+34            cur->child = nullptr;
+35        }
+36
+37        head->prev = nullptr;
+38
+39        return head;
+40
+41    }
+42};
