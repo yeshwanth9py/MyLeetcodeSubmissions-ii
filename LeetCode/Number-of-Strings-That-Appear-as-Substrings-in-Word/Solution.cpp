fa59ace30b2af3b0b0a4a7711bb00a2caa682e86
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-29 12:32:12 +0530

    Add solution for Number of Strings That Appear as Substrings in Word

--- LeetCode/Number-of-Strings-That-Appear-as-Substrings-in-Word/Solution.cpp
+++ LeetCode/Number-of-Strings-That-Appear-as-Substrings-in-Word/Solution.cpp
@@ -7,26 +7,24 @@
 7        int i=0;
 8        int j=-1;
 9        while(i<n){
-10            while(j != -1 && s[i]!=s[j]){
+10            while(j!=-1 && s[i] != s[j]){
 11                j = kmp[j];
 12            }
-13            j++;
-14            i++;
+13            i++;
+14            j++;
 15            kmp[i] = j;
 16        }
-17
-18        for(int i=s1.size(); i<=n; i++){
-19            if(kmp[i] == s1.size()) return 1;
-20        }
-21
-22        return 0;
-23    }
-24
-25    int numOfStrings(vector<string>& pat, string w) {
-26        int cnt = 0;
-27        for(string s: pat){
-28            cnt += compare(s, w);
-29        }
-30        return cnt;
-31    }
-32};
+17        for(int i=s1.size(); i<=n; i++){
+18            if(kmp[i] == s1.size()) return 1;
+19        }
+20        return 0;
+21    }
+22
+23    int numOfStrings(vector<string>& pat, string w) {
+24        int cnt = 0;
+25        for(string s: pat){
+26            cnt += compare(s, w);
+27        }
+28        return cnt;
+29    }
+30};
