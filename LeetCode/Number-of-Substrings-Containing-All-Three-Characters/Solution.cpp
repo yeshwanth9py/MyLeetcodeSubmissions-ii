9855d6ab7befefe62bf2af23b44ac47d320e6703
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-30 11:51:59 +0530

    Add solution for Number of Substrings Containing All Three Characters

--- LeetCode/Number-of-Substrings-Containing-All-Three-Characters/Solution.cpp
+++ LeetCode/Number-of-Substrings-Containing-All-Three-Characters/Solution.cpp
@@ -1,35 +1,47 @@
 1class Solution {
 2public:
 3    int numberOfSubstrings(string s) {
-4        int n = s.size();
-5        unordered_map<char, int> ump;
-6
-7        int i = 0;
-8        int j = -1;
-9        int ans = 0;
-10
-11        while(i<n){
-12            while(j+1<n && (ump[0] == 0 || ump[1] == 0 || ump[2] == 0)){
-13                j++;
-14                ump[s[j]-'a']++;
-15            }
-16
-17            if(j-i+1>0 && ump[0]>0 && ump[1]>0 && ump[2]>0){
-18                // cout<<i<<" "<<j<<" "<<(n-j)<<endl;
-19                ans += (n-j);
-20            }
-21
-22            if(i>j){
-23                i++;
-24                j=i-1;
-25                ump.clear();
-26            }else{
-27                ump[s[i]-'a']--;
-28                i++;
-29            }
-30        }
-31
-32        return ans;
-33
-34    }
-35};
+4        int n = s.length();
+5        int head = -1;
+6        int tail = 0;
+7
+8        int a = 0;
+9        int b = 0;
+10        int c = 0;
+11
+12        int tot = 0;
+13
+14        while(tail<n){
+15            while(head+1<n && (a==0 || b==0 || c==0)){
+16                head++;
+17                if(s[head] == 'a'){
+18                    a++;
+19                }else if(s[head] == 'b'){
+20                    b++;
+21                }else{
+22                    c++;
+23                }
+24            }
+25            if(a>0 && b>0 && c>0){
+26                tot += n-head;
+27            }
+28
+29            if(tail>head){
+30                tail++;
+31                head = tail-1;
+32            }else{
+33                if(s[tail] == 'a'){
+34                    a--;
+35                }else if(s[tail] == 'b'){
+36                    b--;
+37                }else{
+38                    c--;
+39                }
+40                tail++;
+41            }
+42
+43        }
+44
+45        return tot;
+46    }
+47};
