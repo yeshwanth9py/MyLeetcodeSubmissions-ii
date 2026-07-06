4c96fb6ebee33e00b27999886dc461aa59e28015
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-06 19:15:34 +0530

    Add solution for Minimum Operations to Transform Binary String

--- LeetCode/Minimum-Operations-to-Transform-Binary-String/Solution.cpp
+++ LeetCode/Minimum-Operations-to-Transform-Binary-String/Solution.cpp
@@ -1,42 +1,19 @@
-1class Solution {
-2public:
-3    int minOperations(string s1, string s2) {
-4        int n = s1.size();
-5        int i=0;
-6        int ways = 0;
-7        while(i<n){
-8            if(s1[i] == '0'){
-9                if(s2[i] == '1'){
-10                    ways++;
-11                }
-12            }else{
-13                if(s2[i] == '1'){
-14                    //
-15                }else{
-16                    if(i+1<n && s1[i+1] == '1' && s2[i+1] == '0'){
-17                        ways += 1;
-18                        i++;
-19                    }else if(i-1>=0){
-20                        ways+=2;
-21                    }else if(i+1<n && s1[i+1] == '1' && s2[i+1] == '1'){
-22                        ways += 2;
-23                    }else if(i+1<n && s1[i+1] == '0' && s2[i+1] == '0'){
-24                        ways += 2;
-25                        i++;
-26                    }else if(i+1<n && s1[i+1] == '0' && s2[i+1] == '1'){
-27                        ways += 3;
-28                        i++;
-29                    }else if(i-1>=0){
-30                        ways += 3;
-31                        i++;
-32                    }else{
-33                        return -1;
-34                    }
-35                }
-36            }
-37            i++;
-38        }
-39
-40        return ways;
-41    }
-42};
+    int minOperations(string s1, string s2) {
+        if (s1 == "1" && s2 == "0") {
+            return -1;
+        }
+        int res = 0, n = s1.length();
+        for (int i = 0; i < n; i++) {
+            if (s1[i] == s2[i])
+                continue;
+            if (s1[i] == '0') {
+                res++;
+            } else if (i < n - 1) {
+                res += s1[i + 1] == '1' ? 1 : 2;
+                s1[i + 1] = '0';
+            } else {
+                res += 2;
+            }
+        }
+        return res;
+    }
