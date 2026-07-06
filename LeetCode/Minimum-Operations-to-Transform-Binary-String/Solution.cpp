0cf4a5ab5bf9647260e7c3715b3253e9376c9b34
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-06 18:52:36 +0530

    Add solution for Minimum Operations to Transform Binary String

--- LeetCode/Minimum-Operations-to-Transform-Binary-String/Solution.cpp
+++ LeetCode/Minimum-Operations-to-Transform-Binary-String/Solution.cpp
@@ -1,56 +1,42 @@
 1class Solution {
 2public:
-3    int n;
-4    string s1, s2;
-5    int dp[100011][2];
-6    int rec(int ind, int prev){
-7        if(ind == n){
-8            return 0;
-9        }
-10        if(dp[ind][prev] != -1) return dp[ind][prev];
-11        int ways = 1e9;
-12        if(s1[ind] == '0'){
-13            if(s2[ind] == '0'){
-14                ways = min(ways, rec(ind+1, 0));
-15            }else{
-16                ways = min(ways, 1+rec(ind+1, 1));
-17            }
-18        }else{
-19            if(s2[ind] == '1'){
-20                ways = min(ways,  rec(ind+1, 1));
-21            }else{
-22                if(prev == 1){
-23                    ways = min(ways, 2+rec(ind+1, 0));
-24                }else if((ind>0 && prev == 0)){
-25                    ways = min(ways, 2+rec(ind+1, 0));
-26                }
-27
-28                if(ind+1<n && s1[ind+1] == '1'){
-29                    if(s2[ind+1] == '0'){
-30                        ways = min(ways, 1+rec(ind+2, 0));
-31                    }else{
-32                        ways = min(ways, 2+rec(ind+2, 1));
-33                    }
-34                }else if(ind+1<n && s1[ind+1] == '0'){
-35                    if(s2[ind+1] == '0'){
-36                        ways = min(ways, 2+rec(ind+2, 0));
-37                    }else{
-38                        ways = min(ways, 3+rec(ind+2, 1));
-39                    }
-40                }
-41            }
-42        }
-43
-44        return dp[ind][prev] = ways;
-45    }
-46
-47    int minOperations(string s11, string s22) {
-48        s1 = s11;
-49        s2 = s22;
-50        n = s1.size();
-51        memset(dp, -1, sizeof(dp));
-52        int asn = rec(0, 0);
-53        if(asn>=1e8) return -1;
-54        return asn;
-55    }
-56};
+3    int minOperations(string s1, string s2) {
+4        int n = s1.size();
+5        int i=0;
+6        int ways = 0;
+7        while(i<n){
+8            if(s1[i] == '0'){
+9                if(s2[i] == '1'){
+10                    ways++;
+11                }
+12            }else{
+13                if(s2[i] == '1'){
+14                    //
+15                }else{
+16                    if(i+1<n && s1[i+1] == '1' && s2[i+1] == '0'){
+17                        ways += 1;
+18                        i++;
+19                    }else if(i-1>=0){
+20                        ways+=2;
+21                    }else if(i+1<n && s1[i+1] == '1' && s2[i+1] == '1'){
+22                        ways += 2;
+23                    }else if(i+1<n && s1[i+1] == '0' && s2[i+1] == '0'){
+24                        ways += 2;
+25                        i++;
+26                    }else if(i+1<n && s1[i+1] == '0' && s2[i+1] == '1'){
+27                        ways += 3;
+28                        i++;
+29                    }else if(i-1>=0){
+30                        ways += 3;
+31                        i++;
+32                    }else{
+33                        return -1;
+34                    }
+35                }
+36            }
+37            i++;
+38        }
+39
+40        return ways;
+41    }
+42};
