d3460d2af2c3105e618648ce8444683fa1ba1d60
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-06 13:12:29 +0530

    Add solution for Divisible Game

--- LeetCode/Divisible-Game/Solution.cpp
+++ LeetCode/Divisible-Game/Solution.cpp
@@ -1,65 +1,77 @@
-1class Solution {
-2public:
-3    vector<long long> spf;
-4    const long long mod = 1e9+7;
-5    unordered_map<long long, vector<long long>> store;
-6    void pre(){
-7        spf.resize(1e6+10, 0);
-8        for(long long i=2; i<1e6+10; i++){
-9            spf[i] = i;
-10        }
-11
-12        for(long long i=2; i<1e6+10; i++){
-13            if(spf[i] == i){
-14                for(long long j=i*i; j<1e6+10; j+=i){
-15                    if(spf[j] == j) spf[j] = i;
-16                }
-17            }
-18        }
-19    }
-20
-21    vector<long long> getprimes(long long num){
-22        if(store.find(num) != store.end()) return store[num];
-23        vector<long long> all;
-24        int dnum = num;
-25        for(int i=2; i*i<=num; i++){
-26            if(num%i == 0){
-27                while(num%i == 0){
-28                    num = num/i;
-29                }
-30                all.push_back(i);
-31            }
-32        }
-33        if(num>1){
-34            all.push_back(num);
-35        }
-36        return store[dnum] = all;
-37    }
-38
-39    long long divisibleGame(vector<int>& nums) {
-40        long long n = nums.size();
-41        // pre();
-42        long long mxs = -1;
-43        long long mxp = 2;
-44
-45        for(long long i=0; i<n; i++){
-46            unordered_map<long long, long long> ump;
-47            long long tot = 0;
-48            for(long long j=i; j<n; j++){
-49                tot += nums[j];
-50                vector<long long> allprimes = getprimes(nums[j]);
-51                for(long long p: allprimes){
-52                    ump[p] += nums[j];
-53                    int curs = 2*ump[p] - tot; 
-54                    if(curs>mxs || (curs == mxs && mxp>p)){
-55                        mxs = curs;
-56                        mxp = p;
-57                    }
-58                }
-59            }
-60        }
-61
-62        // cout<<mxs<<" "<<mxp;
-63        return ((mxs*mxp)%mod + mod)%mod;
-64    }
-65};
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+using ll   = long long;
+using pii  = pair<int, int>;
+using vi   = vector<int>;
+using vl   = vector<ll>;
+using vvi  = vector<vector<int>>;
+
+#define pb        push_back
+#define all(x)    (x).begin(), (x).end()
+#define sz(x)     (int)(x).size()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+const int inf  = 1e9;
+const ll  linf = 4e18;
+const int mod  = 1e9 + 7;
+
+template <class T> bool ckmin(T& a, const T& b) { return b < a ? (a = b, true) : false; }
+template <class T> bool ckmax(T& a, const T& b) { return a < b ? (a = b, true) : false; }
+
+class Solution {
+public:
+    int divisibleGame(vector<int>& nums) {
+        int n = sz(nums);
+        // A - B -> maximise, smallest k
+        // prod(k, max score)%mod
+        // kadane check for best k
+
+        set<int> candidates;
+
+        for(int x : nums){
+            for(int d=1; d*d <= x; d++){
+                if(x%d) continue;
+                if(d>1)
+                    candidates.insert(d);
+                int opp = x/d;
+                if(opp>1)
+                    candidates.insert(opp);
+            }
+        }
+
+        candidates.insert(2); // handles the case where no candidate divides any element
+
+        auto check = [&](int poss) -> ll{
+            ll res = -inf, cur = -inf;
+
+            for(int x : nums){
+                ll val = x%poss ? -x : x;
+                if(cur == -inf)
+                    cur = val;
+                else
+                    cur = max(val, cur + val);
+                res = max(res, cur);
+            }
+
+            return res;
+        };
+
+        ll prod = 2;
+
+        ll kcur = 2, mxScore = -inf;
+
+        for(auto &x : candidates){
+            ll score = check(x);
+
+            if(score > mxScore){
+                kcur = x;
+                mxScore = score;
+                prod = (score * x) % mod;
+            }
+        }
+
+        return ((((mxScore%mod)+mod)%mod) * kcur) % mod;
+    }
+};
