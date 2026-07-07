e6fe03ff8ca59d0189c325cac04c32523619b9f7
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-07 12:53:30 +0530

    Add solution for Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ LeetCode/Concatenate-Non-Zero-Digits-and-Multiply-by-Sum-I/Solution.cpp
@@ -0,0 +1,21 @@
+1class Solution {
+2public:
+3    long long sumAndMultiply(int n) {
+4        int sm = 0;
+5        string s = "";
+6        while(n>0){
+7            int ld = n%10;
+8            if(ld != 0){
+9                s.push_back(ld+'0');
+10                sm += ld;
+11            }
+12            n = n/10;
+13        }
+14        int val = 0;
+15        if(s!=""){
+16            reverse(s.begin(), s.end());
+17            val = stoi(s);
+18        }
+19        return (1LL*val*sm);
+20    }
+21};
