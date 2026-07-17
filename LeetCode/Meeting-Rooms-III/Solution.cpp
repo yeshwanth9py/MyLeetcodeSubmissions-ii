1f6e6361cd2590654d7a659e8042af399a2a5eab
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-17 11:22:21 +0530

    Add solution for Meeting Rooms III

--- LeetCode/Meeting-Rooms-III/Solution.cpp
+++ LeetCode/Meeting-Rooms-III/Solution.cpp
@@ -1,46 +1,48 @@
 1class Solution {
 2public:
-3    int mostBooked(int n, vector<vector<int>>& arr) {
-4        priority_queue<long long, vector<long long>, greater<long long>> avail;
-5        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> used;
-6
-7        for(long long i=0; i<n; i++) avail.push(i);
-8
-9        sort(arr.begin(), arr.end());
-10
-11        vector<long long> ans(n, 0);
-12
-13        for(auto m: arr){
-14            long long s = m[0];
-15            long long e = m[1];
-16            while(!used.empty() && used.top().first<=s){
-17                auto [et, rm] = used.top(); used.pop();
-18                avail.push(rm);
-19            }
-20
-21            if(avail.size()==0){
-22                auto [et, rm] = used.top(); used.pop();
-23                long long net = 1LL*et+1LL*(e-s);
-24                used.push({net, rm});
-25                // cout<<et<<" "<<rm<<", ";
-26                ans[rm]++;
-27            }else{
-28                long long rm = avail.top(); avail.pop();
-29                used.push({e, rm});
-30                // cout<<s<<" "<<rm<<", ";
-31                ans[rm]++;
-32            }
-33        }
-34
-35        long long asans = 0;
-36        for(long long i=0; i<n; i++){
-37            // cout<<ans[i]<<" ";
-38            if(ans[asans]<ans[i]){
-39                asans = i;
-40            }
-41        }
-42        cout<<endl;
-43        return asans;   
-44
-45    }
-46};
+3    long long mostBooked(long long n, vector<vector<int>>& arr) {
+4        priority_queue<long long, vector<long long>, greater<long long>> avail;  
+5
+6        for(long long i=0; i<n; i++){
+7            avail.push(i);
+8        }
+9
+10        unordered_map<long long, long long> usage;
+11        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> inuse;  //{et, rm}
+12        unordered_map<long long, long long> ump;
+13
+14        sort(arr.begin(), arr.end());
+15
+16        for(auto el: arr){
+17            long long s = el[0];
+18            long long e = el[1];
+19
+20            while(!inuse.empty() && inuse.top().first<=s){
+21                auto [et, rm] = inuse.top(); inuse.pop();
+22                avail.push(rm);
+23            }
+24
+25            if(avail.size() == 0){
+26                auto [et, rm] = inuse.top(); inuse.pop();
+27                long long dura = e-s;
+28                long long aet = et+dura;
+29                inuse.push({aet, rm});
+30                usage[rm]++;
+31            }else{
+32                auto tel = avail.top(); avail.pop();
+33                inuse.push({e, tel});
+34                usage[tel]++;
+35            }
+36        }
+37
+38        long long mxfr = 0;
+39
+40        for(long long i=0; i<n; i++){
+41            if(usage[mxfr]<usage[i]){
+42                mxfr = i;
+43            }
+44        }
+45
+46        return mxfr;
+47    }
+48};
