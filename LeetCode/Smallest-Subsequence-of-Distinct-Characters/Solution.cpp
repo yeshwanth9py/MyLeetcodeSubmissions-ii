62401450cde0a254160a39206696363e5f21b304
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-19 23:07:35 +0530

    Add solution for Smallest Subsequence of Distinct Characters

--- LeetCode/Smallest-Subsequence-of-Distinct-Characters/Solution.cpp
+++ LeetCode/Smallest-Subsequence-of-Distinct-Characters/Solution.cpp
@@ -1,24 +1,29 @@
-1class Solution {
-2public:
-3    string smallestSubsequence(string s) {
-4        unordered_map<char, int> fut;
-5        string ans = "";
-6        unordered_map<char, int> inst;
-7        for(char ch: s){
-8            fut[ch]++;
-9        }
-10        
-11        for(char ch: s){
-12            while(ans.size() > 0 && ans.back()>ch && fut[ans.back()]>0 && inst[ch]==0){
-13                inst[ans.back()]--;
-14                ans.pop_back();
-15            }
-16            fut[ch]--;
-17            if(inst[ch] == 0){
-18                ans.push_back(ch);
-19                inst[ch] = 1;
-20            }
-21        }
-22        return ans;
-23    }
-24};
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int freq[27] = {0};
+        bitset<27> seen;
+        string stack;
+
+        for (auto& c : s)
+            freq[c & 31]++;
+
+        for (auto& c : s) {
+            int x = c & 31;
+            freq[x]--;
+
+            if (seen.test(x))
+                continue;
+
+            while (stack.length() && stack.back() > c && freq[stack.back() & 31]) {
+                seen.reset(stack.back() & 31);
+                stack.pop_back();
+            }
+
+            stack.push_back(c);
+            seen.set(x);
+        }
+
+        return stack;
+    }
+};
