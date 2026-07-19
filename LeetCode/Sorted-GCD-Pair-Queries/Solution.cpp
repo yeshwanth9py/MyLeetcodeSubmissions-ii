9d58215c6b8f7af2549eeaab97e238f749e08996
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-19 14:41:08 +0530

    Add solution for Sorted GCD Pair Queries

--- LeetCode/Sorted-GCD-Pair-Queries/Solution.cpp
+++ LeetCode/Sorted-GCD-Pair-Queries/Solution.cpp
@@ -1,55 +1,47 @@
 1class Solution {
 2public:
 3    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
-4        int mx = *max_element(nums.begin(), nums.end());
-5        int n = nums.size();
-6
-7        vector<int> freq(mx + 1, 0);
-8
-9        for (int ek : nums) {
-10            freq[ek]++;
+4        long long n = nums.size();
+5        long long mx = *(max_element(nums.begin(), nums.end()));
+6        vector<long long> futg(mx+1, 0);
+7
+8        unordered_map<long long, long long> freq;
+9        for(long long el: nums){
+10            freq[el]++;
 11        }
 12
-13        vector<long long> allgcd(mx + 1, 0); // changed to long long
-14
-15        for (int i = 1; i <= mx; i++) {
-16            for (int j = i; j <= mx; j = j + i) { // changed 2*i to i
-17                allgcd[i] += freq[j];
-18            }
-19
-20            long long cnt = allgcd[i];
-21            allgcd[i] = (cnt * (cnt - 1)) / 2;
-22        }
-23
-24        vector<long long> exact(mx + 1, 0); // changed to long long
-25
-26        for (int i = mx; i >= 1; i--) {
-27            exact[i] = allgcd[i];
+13        for(long long i=1; i<=mx; i++){
+14            futg[i] = 0;
+15            for(long long j=i; j<=mx; j=j+i){
+16                futg[i] += freq[j];
+17            }
+18            futg[i] = (futg[i]*(futg[i]-1))/2;
+19        }
+20
+21        vector<long long> exactg(mx+1, 0);
+22        for(long long i=mx; i>=1; i--){
+23            exactg[i] = futg[i];
+24            for(long long j=2*i; j<=mx; j+=i){
+25                exactg[i] -= exactg[j];
+26            }
+27        }
 28
-29            for (int j = 2 * i; j <= mx; j = j + i) {
-30                exact[i] -= exact[j];
-31            }
-32        }
-33
-34        vector<long long> pref(mx + 1, 0); // changed to long long
-35
-36        pref[1] = exact[1];
-37
-38        for (int i = 2; i <= mx; i++) {
-39            pref[i] = pref[i - 1] + exact[i];
-40        }
-41
-42        vector<int> ans;
-43
-44        for (auto q : queries) {
-45            auto it = upper_bound(pref.begin(), pref.end(), q);
-46
-47            // Do not decrement.
-48            int ind = it - pref.begin();
-49
-50            ans.push_back(ind);
-51        }
-52
-53        return ans;
-54    }
-55};
+29        // cout<<exactg[1]<<" ";
+30        for(long long i=2; i<=mx; i++){
+31            // cout<<exactg[i]<<" ";
+32            exactg[i] += exactg[i-1];
+33        }
+34
+35        // cout<<endl;
+36
+37        vector<int> ans;
+38        for(auto q: queries){
+39            q++;
+40            long long ind = lower_bound(exactg.begin(), exactg.end(), q)-exactg.begin();
+41            if(ind == 0) ind = 1;
+42            ans.push_back(ind);
+43        }
+44
+45        return ans;
+46    }
+47};
