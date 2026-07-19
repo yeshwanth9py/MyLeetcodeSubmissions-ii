c641fd0b674f28752e7105e1a9231cf95ab20402
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-19 23:00:40 +0530

    Add solution for Smallest Subsequence of Distinct Characters

--- LeetCode/Smallest-Subsequence-of-Distinct-Characters/Solution.cpp
+++ LeetCode/Smallest-Subsequence-of-Distinct-Characters/Solution.cpp
@@ -1,34 +1,24 @@
 1class Solution {
 2public:
-3    int need;
-4    int n;
-5    string smallestSubsequence(string s) {
-6        unordered_map<char, int> ump;
-7        n = s.size();
-8        for(char ch: s){
-9            ump[ch]++;
-10        }
-11
-12        stack<char> st;
-13        unordered_map<char, int> inst;
-14        for(char ch: s){
-15            while(!st.empty() && st.top()>ch && ump[st.top()]>0 && inst[ch] == 0){
-16                inst[st.top()]--;
-17                st.pop();
-18            }
-19            ump[ch]--;
-20            if(inst[ch]==0){
-21                st.push(ch);
-22                inst[ch]++;
-23            }
-24        }
-25
-26        string ans = "";
-27        while(!st.empty()){
-28            ans += st.top(); st.pop();
-29        }
-30
-31        reverse(ans.begin(), ans.end());
-32        return ans;
-33    }
-34};
+3    string smallestSubsequence(string s) {
+4        unordered_map<char, int> fut;
+5        string ans = "";
+6        unordered_map<char, int> inst;
+7        for(char ch: s){
+8            fut[ch]++;
+9        }
+10        
+11        for(char ch: s){
+12            while(ans.size() > 0 && ans.back()>ch && fut[ans.back()]>0 && inst[ch]==0){
+13                inst[ans.back()]--;
+14                ans.pop_back();
+15            }
+16            fut[ch]--;
+17            if(inst[ch] == 0){
+18                ans.push_back(ch);
+19                inst[ch] = 1;
+20            }
+21        }
+22        return ans;
+23    }
+24};
