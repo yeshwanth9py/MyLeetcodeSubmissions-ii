e3f7381665696c124b90e4f090a25e344ab82099
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-26 16:30:44 +0530

    Add solution for Count Valid Sequences

--- LeetCode/Count-Valid-Sequences/Solution.cpp
+++ LeetCode/Count-Valid-Sequences/Solution.cpp
@@ -1,58 +1,79 @@
-1long long fact[500022];
-2long long invfact[500022];
-3const long long mod = 1e9+7;
-4
-5int cnt = 0;
-6
-7long long binpow(long long x, long long n, long long mod){
-8    if(n == 0) return 1;
-9    long long ans = binpow(x, n/2, mod);
-10    ans = ((ans*ans)%mod + mod)%mod;
-11    if(n%2){
-12        ans = ((ans*x)%mod + mod)%mod;
-13    }
-14    return ans;
-15}
-16
-17
-18void preFact(){
-19    fact[0] = fact[1] = 1;
-20    for(long long i=2; i<500022; i++){
-21        fact[i] = ((fact[i-1]*i)%mod + mod)%mod;
-22    }
-23    
-24    invfact[500022-1] = binpow(fact[500022-1], mod-2, mod);
-25    for(long long i=500022-2; i>=0; i--){
-26        invfact[i] = ((invfact[i+1]*(i+1))%mod + mod)%mod;
-27    }
-28}
+1#include <bits/stdc++.h>
+2using namespace std;
+3
+4class Solution {
+5    static constexpr int MAX_N = 500000;
+6    static constexpr long long MOD = 1'000'000'007LL;
+7
+8    struct Combinations {
+9        long long fact[MAX_N + 1];
+10        long long invFact[MAX_N + 1];
+11
+12        long long binaryPower(long long base, long long exponent) {
+13            long long answer = 1;
+14
+15            while (exponent > 0) {
+16                if (exponent & 1) {
+17                    answer = answer * base % MOD;
+18                }
+19
+20                base = base * base % MOD;
+21                exponent >>= 1;
+22            }
+23
+24            return answer;
+25        }
+26
+27        Combinations() {
+28            fact[0] = 1;
 29
-30long long ncr(long long n, long long r){
-31    if(r<0 || r>n) return 0;
-32    long long num = fact[n];
-33    long long den = ((invfact[n-r] * invfact[r])%mod + mod)%mod;
-34    num = ((num*den)%mod + mod)%mod;
-35    return num;
-36}
-37
-38
-39class Solution {
-40public:
-41    Solution(){
-42        if(cnt == 1) return;
-43        cnt++;
-44        preFact();
-45    }
-46
-47    int countValidSequences(long long n, long long k) {
-48        if(n<k) return 0;
-49        long long tot = ncr(n-1, k-1);
-50        if(n%2 == k%2 && n>=k){
-51            long long extra = (n-k)/2;
-52            extra = ncr(extra+k-1, k-1);
-53            tot = (tot-extra+mod)%mod;
-54        }
-55
-56        return tot;
-57    }
-58};
+30            for (int i = 1; i <= MAX_N; i++) {
+31                fact[i] = fact[i - 1] * i % MOD;
+32            }
+33
+34            invFact[MAX_N] =
+35                binaryPower(fact[MAX_N], MOD - 2);
+36
+37            for (int i = MAX_N; i >= 1; i--) {
+38                invFact[i - 1] =
+39                    invFact[i] * i % MOD;
+40            }
+41        }
+42
+43        long long nCr(int n, int r) {
+44            if (r < 0 || r > n) {
+45                return 0;
+46            }
+47
+48            return fact[n]
+49                * invFact[r] % MOD
+50                * invFact[n - r] % MOD;
+51        }
+52    };
+53
+54    // Created once for the program, not once per Solution object.
+55    inline static Combinations comb;
+56
+57public:
+58    int countValidSequences(long long n, long long k) {
+59        if (n < k) {
+60            return 0;
+61        }
+62
+63        // All compositions of n into k positive elements.
+64        long long answer =
+65            comb.nCr(n - 1, k - 1);
+66
+67        // An all-odd sequence is possible only when n-k is even.
+68        if ((n - k) % 2 == 0) {
+69            long long remaining = (n - k) / 2;
+70
+71            long long allOdd =
+72                comb.nCr(remaining + k - 1, k - 1);
+73
+74            answer = (answer - allOdd + MOD) % MOD;
+75        }
+76
+77        return static_cast<int>(answer);
+78    }
+79};
