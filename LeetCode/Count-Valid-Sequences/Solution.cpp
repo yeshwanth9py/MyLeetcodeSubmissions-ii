74a924eab2833d7f3e33f16f5cbc590017cd5b18
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-26 16:36:23 +0530

    Add solution for Count Valid Sequences

--- LeetCode/Count-Valid-Sequences/Solution.cpp
+++ LeetCode/Count-Valid-Sequences/Solution.cpp
@@ -1,79 +1,73 @@
-1#include <bits/stdc++.h>
-2using namespace std;
-3
-4class Solution {
-5    static constexpr int MAX_N = 500000;
-6    static constexpr long long MOD = 1'000'000'007LL;
-7
-8    struct Combinations {
-9        long long fact[MAX_N + 1];
-10        long long invFact[MAX_N + 1];
-11
-12        long long binaryPower(long long base, long long exponent) {
-13            long long answer = 1;
-14
-15            while (exponent > 0) {
-16                if (exponent & 1) {
-17                    answer = answer * base % MOD;
-18                }
-19
-20                base = base * base % MOD;
-21                exponent >>= 1;
-22            }
-23
-24            return answer;
-25        }
-26
-27        Combinations() {
-28            fact[0] = 1;
-29
-30            for (int i = 1; i <= MAX_N; i++) {
-31                fact[i] = fact[i - 1] * i % MOD;
-32            }
-33
-34            invFact[MAX_N] =
-35                binaryPower(fact[MAX_N], MOD - 2);
-36
-37            for (int i = MAX_N; i >= 1; i--) {
-38                invFact[i - 1] =
-39                    invFact[i] * i % MOD;
-40            }
-41        }
+1long long fact[500022];
+2long long invfact[500022];
+3const long long mod = 1e9+7;
+4
+5
+6long long binpow(long long x, long long n, long long mod){
+7    if(n == 0) return 1;
+8    long long ans = binpow(x, n/2, mod);
+9    ans = ((ans*ans)%mod + mod)%mod;
+10    if(n%2){
+11        ans = ((ans*x)%mod + mod)%mod;
+12    }
+13    return ans;
+14}
+15
+16struct Comb{
+17    void preFact(){
+18        fact[0] = fact[1] = 1;
+19        for(long long i=2; i<500022; i++){
+20            fact[i] = ((fact[i-1]*i)%mod + mod)%mod;
+21        }
+22        
+23        invfact[500022-1] = binpow(fact[500022-1], mod-2, mod);
+24        for(long long i=500022-2; i>=0; i--){
+25            invfact[i] = ((invfact[i+1]*(i+1))%mod + mod)%mod;
+26        }
+27    }
+28    Comb(){
+29        preFact();
+30    }
+31};
+32
+33static Comb cb;
+34
+35long long ncr(long long n, long long r){
+36    if(r<0 || r>n) return 0;
+37    long long num = fact[n];
+38    long long den = ((invfact[n-r] * invfact[r])%mod + mod)%mod;
+39    num = ((num*den)%mod + mod)%mod;
+40    return num;
+41}
 42
-43        long long nCr(int n, int r) {
-44            if (r < 0 || r > n) {
-45                return 0;
-46            }
-47
-48            return fact[n]
-49                * invFact[r] % MOD
-50                * invFact[n - r] % MOD;
-51        }
-52    };
-53
-54    // Created once for the program, not once per Solution object.
-55    inline static Combinations comb;
-56
-57public:
-58    int countValidSequences(long long n, long long k) {
-59        if (n < k) {
-60            return 0;
-61        }
-62
-63        // All compositions of n into k positive elements.
-64        long long answer =
-65            comb.nCr(n - 1, k - 1);
-66
-67        // An all-odd sequence is possible only when n-k is even.
-68        if ((n - k) % 2 == 0) {
-69            long long remaining = (n - k) / 2;
+43
+44
+45
+46
+47class Solution {
+48public:
+49    // Solution(){
+50    //     // if(cnt == 1) return;
+51    //     // cnt++;
+52    //     preFact();
+53    // }
+54
+55    int countValidSequences(long long n, long long k) {
+56        if(n<k) return 0;
+57        // preFact();
+58        long long tot = ncr(n-1, k-1);
+59        if(n%2 == k%2 && n>=k){
+60            long long extra = (n-k)/2;
+61            extra = ncr(extra+k-1, k-1);
+62            tot = (tot-extra+mod)%mod;
+63        }
+64
+65        return tot;
+66    }
+67};
+68
+69
 70
-71            long long allOdd =
-72                comb.nCr(remaining + k - 1, k - 1);
+71
+72
 73
-74            answer = (answer - allOdd + MOD) % MOD;
-75        }
-76
-77        return static_cast<int>(answer);
-78    }
-79};
