bcf9533565da986ebd10d8411b5bf54ef8eb28e1
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-29 19:39:08 +0530

    Add solution for Smallest Palindromic Rearrangement II

--- LeetCode/Smallest-Palindromic-Rearrangement-II/Solution.cpp
+++ LeetCode/Smallest-Palindromic-Rearrangement-II/Solution.cpp
@@ -1,29 +1,29 @@
 1class Solution {
 2public:
-3    const int cap =  1e6+6;
-4    int ncr(int n, int r){
-5        if(r == 0) return 1;
-6        r = min(r, n-r);
-7        long long ans = 1;
-8        for(int i=0; i<r; i++){
-9            ans = (1LL*ans*(n-i))/(i+1);
-10            if(ans>=cap){
-11                ans = cap;
-12            }
-13        }
+3    const long long cap = 1e6+7;
+4    long long ncr(long long n, long long R){
+5        long long ans = 1;
+6        R = min(R, n-R);
+7        for(long long r=0; r<R; r++){
+8            ans = (1LL*ans*(n-r))/(r+1);
+9            if(ans>=cap){
+10                ans = cap;
+11            }
+12        }
+13        if(ans>=cap) ans = cap;
 14        return ans;
 15    }
 16
-17    int countways(vector<int> &ump){
-18        int tot = 0;
-19        for(int i=0; i<26; i++){
-20            if(ump[i]>0){
-21                tot += ump[i];
-22            }
-23        }
-24
-25        long long ways = 1;
-26        for(int i=0; i<26; i++){
+17    int countways(vector<long long> &ump){
+18        long long ways = 1;
+19        long long tot = 0;
+20        for(long long i=0; i<26; i++){
+21            if(ump[i]>0){
+22                tot+=ump[i];
+23            }
+24        }
+25
+26        for(long long i=0; i<26; i++){
 27            if(ump[i]>0){
 28                ways = 1LL*ways*ncr(tot, ump[i]);
 29                if(ways>=cap){
@@ -36,50 +36,53 @@
 36        return ways;
 37    }
 38
-39    string smallestPalindrome(string s, int k) {
-40        vector<int> ump(26, 0);
-41        for(char ch: s){
-42            ump[ch-'a']++;
-43        }
-44        int tot = s.size();
-45        char ext = '-';
-46        for(int i=0; i<26; i++){
-47            if(ump[i]%2 == 1){
-48                ext = char('a'+i);
-49            }
-50            ump[i] = ump[i]/2;
-51        }
-52
-53        if(countways(ump)<k) return "";
-54
-55        int rem = tot/2;
-56        string first = "";
-57        while(rem>0){
-58            for(int i=0; i<26; i++){
-59                if(ump[i]){
-60                    ump[i]--;
-61                    int ways = countways(ump);
-62                    if(ways>=k){
-63                        first += char('a'+i);
-64                        rem--;
-65                        break;
-66                    }else{
-67                        k -= ways;
-68                        ump[i]++;
-69                    }
-70                }
-71            }
-72        }
-73
-74
-75        string sec = first;
-76        reverse(sec.begin(), sec.end());
-77        if(ext != '-'){
-78            first += ext;
-79        }
-80
-81        first = first+sec;
-82
-83        return first;
-84    }
-85};
+39    string smallestPalindrome(string s, long long k) {
+40        vector<long long> ump(26, 0);
+41
+42        for(char ch: s){
+43            ump[ch-'a']++;
+44        }
+45
+46        char ch = '-';
+47        for(long long i=0; i<26; i++){
+48            if(ump[i]>0){
+49                if(ump[i]%2 == 1){
+50                    ch = char('a'+i);
+51                }
+52                ump[i] = ump[i]/2;
+53            }
+54        }
+55
+56        long long tot = s.size()/2;
+57
+58        string f = "";
+59
+60        if(countways(ump)<k) return "";
+61
+62        while(tot>0){
+63            for(long long i=0; i<26; i++){
+64                if(ump[i]>0){
+65                    ump[i]--;
+66                    long long ways = countways(ump);
+67                    if(ways>=k){
+68                        f += char('a'+i);
+69                        tot--;
+70                        break;
+71                    }else{
+72                        k -= ways;
+73                        ump[i]++;
+74                    }
+75                }
+76            }
+77        }
+78
+79        string sec = f;
+80        if(ch != '-'){
+81            f = f+ch;
+82        }
+83        reverse(sec.begin(), sec.end());
+84        f = f+sec;
+85
+86        return f;
+87    }
+88};
