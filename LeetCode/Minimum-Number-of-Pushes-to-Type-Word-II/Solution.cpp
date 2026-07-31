42c9dbe9e771e147418fdf183c4dc7ec889c61e6
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-07-31 10:18:53 +0530

    Add solution for Minimum Number of Pushes to Type Word II

--- LeetCode/Minimum-Number-of-Pushes-to-Type-Word-II/Solution.cpp
+++ LeetCode/Minimum-Number-of-Pushes-to-Type-Word-II/Solution.cpp
@@ -2,26 +2,23 @@
 2public:
 3    int minimumPushes(string word) {
 4        priority_queue<int, vector<int>, greater<int>> pq;
-5        for(int i=2; i<=9; i++) pq.push(1);
-6        priority_queue<int> pq2;
-7        vector<int> all(26, 0);
-8        for(char ch: word){
-9            all[ch-'a']++;
-10        }
-11
-12        for(int i=0; i<26; i++){
-13            pq2.push({all[i]});
-14        }
-15
-16        int ans = 0;
-17        while(!pq2.empty()){
-18            auto tel = pq2.top(); pq2.pop();
-19            auto noty = pq.top(); pq.pop();
-20            ans += tel*noty;
-21            noty++;
-22            pq.push(noty);
-23        }
-24
-25        return ans;
-26    }
-27};
+5        for(int i=2; i<=9; i++){
+6            pq.push(1);
+7        }
+8        vector<int> alph(26, 0);
+9        for(char ch: word){
+10            alph[ch-'a']++;
+11        }
+12
+13        sort(alph.rbegin(), alph.rend());
+14        int ans = 0;
+15        for(int i=0; i<26; i++){
+16            auto tel = pq.top(); pq.pop();
+17            ans += alph[i]*tel;
+18            tel++;
+19            pq.push(tel);
+20        }
+21
+22        return ans;
+23    }
+24};
