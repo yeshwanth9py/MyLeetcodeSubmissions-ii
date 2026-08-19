b2ec36f5fcc7058653abcfa6dfa1c5f8c395e41f
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-08-19 17:22:51 +0530

    Add solution for Maximum Value of Concatenated Binary Segments

--- LeetCode/Maximum-Value-of-Concatenated-Binary-Segments/Solution.cpp
+++ LeetCode/Maximum-Value-of-Concatenated-Binary-Segments/Solution.cpp
@@ -25,55 +25,48 @@
 25        }
 26
 27        sort(vec.begin(), vec.end(), [&](pair<long long, long long> &a, pair<long long, long long> &b){
-28            // if(a.second == 0 || b.second == 0){
-29            //     return a.second<b.second;
-30            // }
+28            if(a.second == 0 || b.second == 0){
+29                return a.second<b.second;
+30            }
 31            // if(a.first>0 && a.second>0 && b.first>0 && b.second>0){
 32            //     return a.second<b.second;
 33            // }
 34            // return a.first>b.first;
 35
-36                // Case 1:
-37                // Segments having only 1s come first.
-38                if (a.second == 0 && b.second != 0)
-39                    return true;
-40
-41                if (a.second != 0 && b.second == 0)
-42                    return false;
-43
-44                // Both are only 1s.
-45                if (a.second == 0 && b.second == 0)
-46                    return a.first > b.first;
-47
-48                // Case 2:
-49                // Segments having only 0s go last.
-50                if (a.first == 0 && b.first != 0)
-51                    return false;
-52
-53                if (a.first != 0 && b.first == 0)
-54                    return true;
-55
-56                // Both have at least one 1 and one 0.
-57                // More 1s first.
-58                if (a.first != b.first)
-59                    return a.first > b.first;
-60
-61                // Same number of 1s -> fewer 0s first.
-62                return a.second < b.second;
-63        });
-64
-65        long long ans = 0;
-66
-67        for(long long i=0; i<n; i++){
-68            auto [no1, no0] = vec[i];
-69            // long long curv = (1<<ans-1) + (1<<ans-2) + (1<<ans-3)
-70            long long curv = ((binpow(2, tot)) - (binpow(2, tot-no1)) + mod)%mod;
-71            tot -= (no1+no0);
-72            ans += curv;
-73            ans = (ans%mod + mod)%mod;
-74        }
-75
-76        return ans;
-77
-78    }
-79};
+36                // Both are only 1s.
+37                if (a.second == 0 && b.second == 0){ 
+38                    return a.first > b.first;
+39                }
+40                // Case 2:
+41                // Segments having only 0s go last.
+42                if (a.first == 0 && b.first != 0){
+43                    return false;
+44                }
+45                if (a.first != 0 && b.first == 0){
+46                    return true;
+47                }
+48                // Both have at least one 1 and one 0.
+49                // More 1s first.
+50                if (a.first != b.first){
+51                    return a.first > b.first;
+52                }
+53
+54                // Same number of 1s -> fewer 0s first.
+55                return a.second < b.second;
+56        });
+57
+58        long long ans = 0;
+59
+60        for(long long i=0; i<n; i++){
+61            auto [no1, no0] = vec[i];
+62            // long long curv = (1<<ans-1) + (1<<ans-2) + (1<<ans-3)
+63            long long curv = ((binpow(2, tot)) - (binpow(2, tot-no1)) + mod)%mod;
+64            tot -= (no1+no0);
+65            ans += curv;
+66            ans = (ans%mod + mod)%mod;
+67        }
+68
+69        return ans;
+70
+71    }
+72};
