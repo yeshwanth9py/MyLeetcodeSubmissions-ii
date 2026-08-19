fefdd72748fbc0ec5f5e8fc6d29752cd1ffd21f0
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-08-19 12:09:50 +0530

    Add solution for Minimum Initial Energy to Finish Tasks

--- LeetCode/Minimum-Initial-Energy-to-Finish-Tasks/Solution.cpp
+++ LeetCode/Minimum-Initial-Energy-to-Finish-Tasks/Solution.cpp
@@ -1,49 +1,32 @@
 1class Solution {
 2public:
 3    int minimumEffort(vector<vector<int>>& arr) {
-4        int n = arr.size();
-5        sort(arr.begin(), arr.end(), [&](vector<int> &a1, vector<int> &a2){
-6            return max(a1[1], a1[0]+a2[1]) < max(a2[1], a2[0]+a1[1]);
-7        });
-8
-9        auto check = [&](int md){
-10            int tot = md;
-11            for(int i=0; i<arr.size(); i++){
-12                if(arr[i][1]<=tot){
-13                    tot -= arr[i][0];
-14                }else{
-15                    return 0;
-16                }
-17            }
-18
-19            return 1;
-20        };
-21
-22        int lo = 0;
-23        int hi = 1e9;
-24        int ans = -1;
-25
-26        while(lo<=hi){
-27            int mid = (lo+hi)/2;
-28            if(check(mid)){
-29                ans = mid;
-30                hi = mid-1;
-31            }else{
-32                lo = mid+1;
-33            }
-34        }
-35
-36        while(1){
-37            if(check(ans-1)){
-38                ans = ans - 1;
-39            }else{
-40                break;
-41            }
-42        }
-43
-44        return ans;
-45
-46
-47
-48    }
-49};
+4        sort(arr.begin(), arr.end(), [&](vector<int> &a, vector<int> &b){
+5            return max(a[0]+b[1], a[1]) < max(b[0]+a[1], b[1]);
+6        });
+7
+8        int sm = 0;
+9        for(int i=0; i<arr.size(); i++){
+10            sm += arr[i][0];
+11        }
+12
+13        if(arr[0][1]>sm){
+14            sm = arr[0][1];
+15        }
+16
+17        int act = sm;
+18        for(int i=0; i<arr.size(); i++){
+19            if(sm>=arr[i][1]){
+20                sm -= arr[i][0];
+21            }else{
+22                act += (arr[i][1]-sm);
+23                sm = arr[i][1] - arr[i][0];
+24            }
+25        }
+26        // if(sm == 0){
+27        //     sm++;
+28        // }
+29
+30        return act;
+31    }
+32};
