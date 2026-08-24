2e549524f4614eaf72607ef55be359bf6b258157
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-08-24 12:26:40 +0530

    Add solution for Sum Game

--- LeetCode/Sum-Game/Solution.cpp
+++ LeetCode/Sum-Game/Solution.cpp
@@ -1,34 +1,30 @@
 1class Solution {
 2public:
-3    bool sumGame(string num) {
-4        int l1 = 0;
-5        int l2 = 0;
-6        int r1 = 0;
-7        int r2 = 0;
-8        int n = num.size();
-9
-10        int totch = 0;
-11        for(int i=0; i<n; i++){
-12            if(i<n/2){
-13                if(num[i] == '?'){
-14                    l2+=9;
-15                    totch++;
-16                }else{
-17                    l1 += (num[i]-'0');
-18                    l2 += (num[i] - '0');
-19                }
-20            }else{
-21                if(num[i] == '?'){
-22                    r2+=9;
-23                    totch++;
-24                }else{
-25                    r1 += (num[i]-'0');
-26                    r2 += (num[i]-'0');
-27                }
-28            }
-29        }
-30
-31        // Bob can only win if (2 * left_sum + 9 * left_questions) == (2 * right_sum + 9 * right_questions)
-32        return l1 + l2 != r1 + r2;
-33    }
-34};
+3    bool sumGame(string s) {
+4        int n = s.size();
+5        int s1 = 0;
+6        int s2 = 0;
+7        int q1 = 0;
+8        int q2 = 0;
+9        for(int i=0; i<n; i++){
+10            if(i<(n/2)){
+11                if(s[i] == '?'){
+12                    q1++;
+13                    continue;
+14                }
+15                s1 += s[i]-'0';
+16            }else{
+17                if(s[i] == '?'){
+18                    q2++;
+19                    continue;
+20                }
+21                s2 += s[i]-'0';
+22            }
+23        }
+24
+25        if((s1-s2)*2 == 9*(q2-q1)){
+26            return 0;
+27        }
+28        return 1;
+29    }
+30};
