8612ccabffb917b791589e19f519d415ae429811
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-09-10 00:49:04 +0530

    Add solution for Count Commas in Range II

--- LeetCode/Count-Commas-in-Range-II/Solution.cpp
+++ LeetCode/Count-Commas-in-Range-II/Solution.cpp
@@ -2,8 +2,8 @@
 2public:
 3    long long countCommas(long long n) {
 4        if(n<1000) return 0;
-5        long long ans = 0;
-6        long long mul = 1e3;
+5        long long mul = 1e3;
+6        long long ans = 0;
 7        while(n>=mul){
 8            ans += (n-mul+1);
 9            mul = mul*1e3;
