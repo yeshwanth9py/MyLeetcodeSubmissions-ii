8ac14e204d76bafdbaf59241d5a22dcfa1190317
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-09-12 12:54:19 +0530

    Add solution for Coin Change II

--- LeetCode/Coin-Change-II/Solution.cpp
+++ LeetCode/Coin-Change-II/Solution.cpp
@@ -1,32 +1,21 @@
-1class Solution {
-2public:
-3    vector<int> arr;
-4    unsigned int dp[301][5001];
-5    int amt;
-6
-7    int rec(){
-8        for(int ind=arr.size(); ind>=0; ind--){
-9            for(int rem=0; rem<=amt; rem++){
-10                if(ind == arr.size()){
-11                    dp[ind][rem] = (rem==0);
-12                    continue;
-13                }
-14
-15                unsigned int dontake = dp[ind+1][rem];
-16                unsigned int take = (rem-arr[ind]>=0 ? dp[ind][rem-arr[ind]] : 0);
-17
-18                if(dontake > INT_MAX - take)
-19                    dp[ind][rem] = INT_MAX;
-20                else
-21                    dp[ind][rem] = dontake + take;
-22            }
-23        }
-24        return dp[0][amt];
-25    }
-26
-27    int change(int amount, vector<int>& coins) {
-28        arr = coins;
-29        amt = amount;
-30        return rec();
-31    }
-32};
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+
+        // Base case: 1 way to make amount 0 (empty set)
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    // Inclusion: use current coin (dp[i]) + Exclusion: skip current coin (dp[i-1])
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+}
